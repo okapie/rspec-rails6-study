326708faba6cd8657769b60edaa0489caba009a0
okapie <tatigreuze@yahoo.co.jp>
2022-10-22 20:39:15 +0900

    Replace local variable with `let`.   * Expect result: 1 example, 0 failures.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   subject { 'Item1' }
 
-  it('`name`を登録すると、`name`が取得出来ること') do
-    item = Item.new('Item1')
+  let(:item) { Item.new('Item1') }
 
+  it('`name`を登録すると、`name`が取得出来ること') do
     expect(item.name).to eq 'Item1'
     # 次の書き方でも同じ結果が得られる。
     # expect(item.name).to eq('Item1')
