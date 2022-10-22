d23adad255124349d1a6cea2f7c8341ba92bc234
okapie <tatigreuze@yahoo.co.jp>
2022-10-22 10:09:29 +0900

    Delete source code.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,2 @@
 class Item < ApplicationRecord
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def introduction
-    cocotte_minute = "Cocotte-Minute"
-    p "This is the #{cocotte_minute}."
-  end
 end
-
-item = Item.new
-item.introduction # Expect to output `This is the Cocotte-Minute.`
