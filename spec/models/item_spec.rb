f6ce8d9fdf4c99fde6ccecb4617796494bd1238a
okapie <tatigreuze@yahoo.co.jp>
2022-10-23 20:35:03 +0900

    Replace `Item` with `described_class`.   * Expect result: 1 example, 0 failures.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   subject { 'Item1' }
 
-  let!(:item) { Item.create(name: 'Item1') }
+  let!(:item) { described_class.create(name: 'Item1') }
 
   # 遅延初期化が行われるので、NoMethodError になる。
   # let(:created_item) { Item.create(name: 'Item1') }
 
   it('`item`を登録すると、`item`が取得出来ること') do
-    expect(Item.first).to eq item
+    expect(described_class.first).to eq item
   end
 end
