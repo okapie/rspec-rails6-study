75df746685278db91b111c55766ed55819b96a7f
okapie <tatigreuze@yahoo.co.jp>
2022-11-03 20:59:27 +0900

    Add a sample of controller test in using `raise_error`.
    * Expect result: 1 example, 0 failures.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+  context 'POST リクエスト' do
+    it '`name` に `nil` を設定した POST リクエストは、例外が発生する' do
+      expect{Item.create!(name: nil)}.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
 end
