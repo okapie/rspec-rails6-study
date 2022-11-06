c1e7f16430a1583a40b824db0bce8e78e7cb4d69
okapie <tatigreuze@yahoo.co.jp>
2022-11-06 20:47:39 +0900

    Add valid pattern to `spec/models/item_spec.rb`.
    * Expect result: 1 example, 0 failures.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,21 +1,12 @@
 require 'rails_helper'
 
-shared_context 'first item' do
-  before do
-    described_class.create(name: 'Item1')
-  end
-end
-
 RSpec.describe Item, type: :model do
-  subject { 'Item1' }
+  fixtures :items
 
-  include_context 'first item'
-
-  it('`item`を登録すると、その`name`が取得出来ること') do
-    expect(described_class.first.name).to eq('Item1')
-  end
-
-  it('`item`の`name`が`Item2`ではないこと') do
-    expect(described_class.first.name).not_to eq('Item2')
+  context '有効な値' do
+    it '`name` に文字列を設定した場合、有効と判定される' do
+      @item = items(:item_success_post);
+      expect(@item).to be_valid
+    end
   end
 end
