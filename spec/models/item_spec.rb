66e472870e42eca0fc13540181614e49e222fbb0
okapie <tatigreuze@yahoo.co.jp>
2022-11-13 19:47:52 +0900

    Add a sample of `shared_examples` and `it_behaves_like`.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   fixtures :items
 
-  context '有効な値' do
-    it '`name` に文字列を設定した場合、有効と判定される' do
-      @item = items(:item_success_post);
-      expect(@item).to be_valid
+  shared_examples 'validation' do
+    it '必須項目の検証' do
+      expect(item).to be_valid
     end
+  end
 
+  context '有効な値' do
+    # let は、example group (describe または context) に置く必要がある。
+    let(:item) { items(:item_success_post) }
+    # it_behaves_like は、example group (describe または context) に置く必要がある。
+    it_behaves_like 'validation'
+  end
+
+  context '無効な値' do
     it '`name` に空文字を設定した場合、無効と判定される' do
       @item = items(:item_failed_post);
       expect(@item).to be_invalid
