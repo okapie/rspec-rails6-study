7fd01596637016c279092236eb8d2e9a75bf811e
okapie <tatigreuze@yahoo.co.jp>
2022-11-26 21:36:04 +0900

    Add a test case to request `GET /users/{:id}/activate.json`.
      * Expect result: 1 example, 0 failures.

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+shared_context 'first user' do
+  before do
+    User.create(name: 'User1')
+  end
+
+  after do
+    User.delete_all
+  end
+end
+
+RSpec.describe UsersController, type: :request do
+  include_context 'first user'
+  let(:id) { User.first.id }
+
+  describe 'GET /users/{:id}/activate.json' do
+    subject { get "/users/#{id}/activate.json" }
+
+    context 'クエリなし' do
+      it 'リクエスト成功 (status 200)' do
+        expect(subject).to eq 200
+      end
+    end
   end
 end
